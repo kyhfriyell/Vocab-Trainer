5e7633c879b6ad6e868128dc673d2c892118ad37
kyhfriyell <kyhfriyell.lab@gmail.com>
2026-02-04 22:19:36 +0100

    404 page and navigation script added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withComponentInputBinding, withRouterConfig } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withHashLocation, withRouterConfig } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes,  withRouterConfig({paramsInheritanceStrategy: "always"})),
-    // withRouterConfig injects dynamic route parameters in children routes as input
-    
+    provideRouter(
+      routes,  
+      withRouterConfig({
+        paramsInheritanceStrategy: "always"
+      }),
+      // withRouterConfig injects dynamic route parameters in children routes as input
+      withHashLocation() // this is to make routes work on github pages)
+    )
   ]
 };
